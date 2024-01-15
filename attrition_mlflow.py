bb294c14c9f64092765e138077d0e86163741365
Rohith <rohithravi2015@gmail.com>
2024-01-16 05:12:56 +0530

    updated

--- attrition_mlflow.py
+++ attrition_mlflow.py
@@ -45,7 +45,7 @@ def fn_mlflow(model,X_train,X_test,y_train,y_test,model_list):
     else:
         mlflow.sklearn.log_model(model, "model")
    # Define the experiment name
-    experiment_name = "exp_attrition"
+    experiment_name = "exp_attrition1"
 
     # Check if the experiment exists
     experiment = mlflow.get_experiment_by_name(experiment_name)
@@ -89,8 +89,8 @@ def fn_mlflow(model,X_train,X_test,y_train,y_test,model_list):
             print(f"{model} accuracy: {accuracy}")
             print(f"{model} F1 score: {f1}")
     
-            # end current run
-            mlflow.end_run()
+    # end current run
+    mlflow.end_run()
 
     # Register the model
     # model_details = mlflow.register_model(model_uri="mlflow-artifacts:/789301157474710172/3bbec4cffcb547dc997e2a2c2196b73d/artifacts/model"
