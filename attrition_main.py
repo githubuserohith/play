644fa9374bc48d1a835db3686e1158a0482fced6
Rohith <rohithravi2015@gmail.com>
2024-01-17 02:12:58 +0530

    bg

--- attrition_main.py
+++ attrition_main.py
@@ -4,8 +4,19 @@ import attrition_streamlit as ast
 import os
 import pandas as pd
 import warnings
+import streamlit as st
 
 warnings.filterwarnings("ignore")
+
+css = """
+    body {
+        background-image: url('Photo-compressed.png');
+        background-size: cover;
+    }
+"""
+
+# Set the custom CSS
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
 # os.chdir(r"")
 df = pd.read_csv("IBM.csv")
